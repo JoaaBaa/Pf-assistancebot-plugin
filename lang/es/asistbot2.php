2f61d75dffd611bb3e34f76fd2dfa6119a0414f3
abraamch <107865866+abraamch@users.noreply.github.com>
2024-06-10 01:04:00 -0300

    Update asistbot2.php

--- lang/es/asistbot2.php
+++ lang/es/asistbot2.php
@@ -26,12 +26,18 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'ASISTBOT2';
+$string['modulename'] = 'AsistBot2';
+$string['modulenameplural'] = 'AsistBots2';
+
+//on-off del bot
+$string['ison'] = 'Plugin Encendido';
+$string['ison_help'] = 'Tildar si se quiere que el recurso esté procesando las asistencias.';
 
 //Porcentaje acistencias y pedido de cámara
-$string['attendancepercentage'] = 'Tiempo de asistencia';
-$string['attendancepercentage_help'] = 'Ingrese en minutos el valor mínimo de asistencia Requerido para el curso.';
-$string['invalidattendancepercentage'] = 'Rango de asistencia inválido. Ingrese un número válido.';
-$string['attendancepercentagerange'] = 'El rango de asistencia debe estar entre 75% y 100%.';
+$string['attendancepercentage'] = 'Porcentaje de asistencia';
+$string['attendancepercentage_help'] = 'Ingrese el porcentaje mínimo de asistencia Requerido para el curso.';
+$string['invalidattendancepercentage'] = 'Rango de asistencia inválido. Ingrese un porcentaje válido entre 75 y 95.';
+$string['attendancepercentagerange'] = 'El rango de asistencia debe estar entre 75% y 95%.';
 
 $string['requirecamera'] = 'Requiere cámara';
 $string['requirecamera_help'] = 'Marcar si la camara debe estar encendida';
@@ -39,9 +45,13 @@ $string['requirecamera_help'] = 'Marcar si la camara debe estar encendida';
 //Tarea programada
 $string['tarea_programada'] = 'Tarea Programada de Asistbot2';
 
+//Evaluación de asistencias
+$string['evaluatetolerance'] = 'Evaluar tiempo de tolerancia';
+$string['evaluatetolerance_help'] = 'Marcar si el curso evaluará el tiempo de llegada de los alumnos';
+
 $string['tolerancetime'] = 'Tiempo de tolerancia de retraso';
 $string['tolerancetime_help'] = 'Tiempo de gracia para que el alumno no pierda el presente';
-$string['tolerancetimelimit'] = 'El tiempo de tolerancia no puede ser mayor a 25 minutos';
+$string['tolerancetimelimit'] = 'De evaluarse, el tiempo de tolerancia no puede ser mayor a 25 minutos ni inferior a 1 minuto.';
 $string['starttime'] = 'Hora de inicio';
 $string['starttime_help'] = 'Hora de inicio de la clase';
 $string['endtime'] = 'Hora de finalización';
@@ -51,3 +61,8 @@ $string['endtimegreater'] = 'La hora de finalización debe ser mayor que la hora
 $string['executionhour'] = 'Hora de ejecución';
 $string['executionhour_help'] = 'Hora en la cual la tarea programada deba ejecutarse (0-23).';
 $string['invalidexecutionhour'] = 'La hora debe estar entre 0 y 23.';
+
+// Duración de la clase
+$string['classlength'] = 'Duración de clase';
+$string['classlength_help'] = 'Ingrese la duración de la clase en minutos.';
+$string['invalidclasslength'] = 'Duración inválida. Ingrese un valor representativo de 1 a 240 minutos.';
